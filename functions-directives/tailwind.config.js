006db4d3f91c701b91b51869eb4111b5504420b8
Shtekata <gesheval@gmail.com>
2025-06-30 02:33:03 +0300

    functions-directives-tailwind

---
+++ functions-directives/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+    },
+  },
+  plugins: [],
+}
